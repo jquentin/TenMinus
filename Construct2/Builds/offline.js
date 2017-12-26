fcb6cc0718c61dd3a3df1d7b224de6f079a982b3
Jeremy Quentin <jeremy.quentin@gmail.com>
2017-12-26 22:06:32 +0100

    Add coins everywhere

---
+++ Construct2/Builds/offline.js
@@ -0,0 +1,22 @@
+﻿{
+	"version": 1514321091,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/card-sheet0.png",
+		"images/card-sheet1.png",
+		"images/table-sheet0.png",
+		"images/waitingwheel-sheet0.png",
+		"images/coin-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png",
+		"cards.png",
+		"channel.html"
+	]
+}
